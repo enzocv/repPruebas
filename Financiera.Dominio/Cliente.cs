8639da784df9ec216370f981ff2a75d89d4a5e88
p-cuadros <patrick_jcq@hotmail.com>
2017-03-10 17:06:29 -0500

    Agregar archivos de proyecto.

---
+++ Financiera.Dominio/Cliente.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Financiera.Dominio
+{
+    public class Cliente
+    {
+        #region Propiedades
+        public int CodigoCliente { get; set; }
+        public string NombreCliente { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+
+        #endregion
+        
+        #region Metodos
+        public static Cliente Crear(int ai_cod_cliente
+            , string as_nom_cliente, DateTime adt_fec_nacimiento)
+        {
+            return new Cliente()
+            {
+                CodigoCliente = ai_cod_cliente,
+                NombreCliente = as_nom_cliente,
+                FechaNacimiento = adt_fec_nacimiento
+            };
+        }
+
+        public void ModificarNombre(string as_nuevo_nombre)
+        {
+            NombreCliente = as_nuevo_nombre;
+        } 
+        #endregion
+    }
+}
---
+++ Financiera.Dominio/CuentaAhorros.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Financiera.Dominio
+{
+    public class CuentaAhorros
+    {
+        public int NumeroCuenta { get; set; }
+        public string CodigoCuenta { get; set; }
+        public decimal SaldoDisponible { get; set; }
+        public int CodigoCliente { get; set; }
+        public virtual Cliente Propietario { get; set; }
+        public byte EstadoCuenta { get; set; }
+    }
+}
