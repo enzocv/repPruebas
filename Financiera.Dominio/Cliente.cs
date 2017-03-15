cc6788c96352f160cf54074a9f3df25cfcca9336
p-cuadros <patrick_jcq@hotmail.com>
2017-03-15 16:20:19 -0500

    version 2

--- Financiera.Dominio/Cliente.cs
+++ Financiera.Dominio/Cliente.cs
@@ -6,16 +6,33 @@ using System.Threading.Tasks;
 
 namespace Financiera.Dominio
 {
+    /// <summary>
+    /// Clase que representa un cliente
+    /// </summary>
     public class Cliente
     {
         #region Propiedades
-        public int CodigoCliente { get; set; }
-        public string NombreCliente { get; set; }
-        public DateTime FechaNacimiento { get; set; }
+        /// <summary>
+        /// Propiedad que representa el Código del Cliente
+        /// </summary>
+        public int CodigoCliente { get; private set; }
+        public string NombreCliente { get; private set; }
+        public DateTime FechaNacimiento { get; private set; }
 
         #endregion
-        
+
+        #region Constructor
+        private Cliente() { }
+        #endregion
+
         #region Metodos
+        /// <summary>
+        /// Clase estatica que crea una nueva instancia de un cliente
+        /// </summary>
+        /// <param name="ai_cod_cliente">Código del cliente</param>
+        /// <param name="as_nom_cliente">Nombre del cliente</param>
+        /// <param name="adt_fec_nacimiento">Fecha de nacimiento</param>
+        /// <returns>Objeto que representa una nueva instancia de la clase Cliente</returns>
         public static Cliente Crear(int ai_cod_cliente
             , string as_nom_cliente, DateTime adt_fec_nacimiento)
         {
--- Financiera.Dominio/CuentaAhorros.cs
+++ Financiera.Dominio/CuentaAhorros.cs
@@ -8,11 +8,53 @@ namespace Financiera.Dominio
 {
     public class CuentaAhorros
     {
-        public int NumeroCuenta { get; set; }
-        public string CodigoCuenta { get; set; }
-        public decimal SaldoDisponible { get; set; }
-        public int CodigoCliente { get; set; }
-        public virtual Cliente Propietario { get; set; }
-        public byte EstadoCuenta { get; set; }
+
+        #region Propiedades
+        public int NumeroCuenta { get; private set; }
+        public string CodigoCuenta { get; private set; }
+        public decimal SaldoDisponible { get; private set; }
+        public int CodigoCliente { get; private set; }
+        public virtual Cliente Propietario { get; private set; }
+        public byte EstadoCuenta { get; private set; }
+
+        #endregion
+
+        #region Constructor
+        private CuentaAhorros() { }
+
+        #endregion
+
+        #region Metodos
+
+        public static CuentaAhorros Aperturar(string as_cod_cuenta
+            , Cliente ao_propietario)
+        {
+            return new CuentaAhorros()
+            {
+                CodigoCuenta = as_cod_cuenta,
+                SaldoDisponible = 0,
+                CodigoCliente = ao_propietario.CodigoCliente,
+                Propietario = ao_propietario,
+                EstadoCuenta = 1
+            };
+        }
+
+        public void Depositar(decimal monto)
+        {
+            SaldoDisponible += monto;
+        }
+
+        public void Retirar(decimal monto)
+        {
+            SaldoDisponible -= monto;
+        }
+
+        public void Cancelar()
+        {
+            SaldoDisponible = 0;
+            EstadoCuenta = 5;
+        }
+
+        #endregion
     }
 }
